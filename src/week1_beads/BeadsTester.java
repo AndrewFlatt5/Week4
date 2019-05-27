dcf8547332fc58e961ed3643771728d7776e85a1
andrew <flatta@sheridancollege.ca>
2019-05-27 12:30:56 -0400

    Beads Week 4

---
+++ src/week1_beads/BeadsTester.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week1_beads;
+
+/**
+ *
+ * @author AndyR
+ */
+public class BeadsTester {
+    public static void main(String[] args) {
+        Week1_Beads b1 = new Week1_Beads();
+       // b1.setColor("white");
+        //b1.setLetter('a');
+        
+        //System.out.println();
+        System.out.println(b1.getColor()+" "+b1.getLetter());
+        
+       
+    
+    }
+    
+}
+
---
+++ src/week1_beads/Week1_Beads.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week1_beads;
+
+/**
+ *
+ * @author AndyR
+ */
+public class Week1_Beads {
+
+    /**
+     * @param args the command line arguments
+     */
+    private String color;
+    private char letter;
+    
+    Week1_Beads()
+    {
+        color = "black";
+        letter = 'b';
+        //week1_Beads(String clr);
+    }
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String clr) {
+        color = clr;
+    }
+
+    /**
+     * @return the letter
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * @param letter the letter to set
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+    public static void main(String[] args) {
+        int num1 = 0;
+        for(int i=0; i>=50; i+=10)
+            System.out.println(num1);
+    }
+        
+    
+}
